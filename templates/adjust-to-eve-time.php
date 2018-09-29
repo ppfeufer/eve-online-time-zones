8eac95bb7bd6792d605b04d06ce3437aa9a9e74f
Peter Pfeufer <ppfeufer@gmail.com>
2018-09-29 12:50:52 +0200

    License header updated

--- templates/adjust-to-eve-time.php
+++ templates/adjust-to-eve-time.php
@@ -1,3 +1,23 @@
+<?php
+
+/**
+ * Copyright (C) 2017 Rounon Dax
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+?>
 <p class="text-right">
 	<button id="btnadjust" class="btn btn-info btn-xs hidden" onclick="showAdjust();"><?php echo \__('Adjust Time', 'eve-online-time-zones'); ?></button>
 	<button id="btnclear" class="btn btn-danger btn-xs hidden" onclick="window.location.hash = '';"><?php echo \__('Remove', 'eve-online-time-zones'); ?></button>
